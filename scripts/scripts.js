904bbb7a5add2c00588b0c459d4b0504a28d8ea9
Rootdiv <rootdiv@mail.ru>
2021-01-31 11:56:15 +0300

    feat: Lesson02 Required task

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,10 +1,7 @@
-let money = 5000;
-let income = 5000;
-let addExpenses = 500;
-let deposit = 10000;
-const mission = 'Изучить JavaScript на курсе';
-const period = '2 месяца';
-
-alert('Привет!');
-
-console.log('Курс JavaScript');
+//Усложнённое задание
+'use strict';
+const num = 266219;
+const numArr = num.toString().split('');
+const result = numArr.map(Number).reduce((res, current) => res * current, 1);
+const degree = result ** 3;
+console.log(degree.toString().slice(0, 2));
