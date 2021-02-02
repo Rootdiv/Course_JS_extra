7a3dbb113fda49e9116fe036b9f6f576d91231f9
Rootdiv <rootdiv@mail.ru>
2021-02-02 21:02:16 +0300

    feat: Lesson04 Complicated task

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,33 +1,16 @@
 //Усложнённое задание
-const lang = prompt('Ваш язык? | Your language?', 'ru');
-const langArray = {
-  'ru': ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'],
-  'en': ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
-};
+'use strict';
+const strInput = prompt('Введите произвольный текст');
 
-if (lang === 'ru') {
-  console.log(langArray.ru);
-} else if (lang === 'en') {
-  console.log(langArray.en);
-} else {
-  console.log('Your language is undefined');
+function strChange(str) {
+  if (typeof str === 'string' && str !== '') {
+    if (str.length > 30) {
+      return str.substring(0, 30).trim().concat('...');
+    } else {
+      console.log('"' + str + '"');
+      return '"' + str.trim() + '"';
+    }
+  }
+  return 'Это не строка';
 }
-
-switch (lang) {
-  case 'ru':
-    console.log(langArray.ru);
-    break;
-  case 'en':
-    console.log(langArray.en);
-    break;
-  default:
-    console.log('Your language is undefined');
-}
-
-console.log(langArray[lang]);
-
-const namePerson = prompt('Ваше имя?');
-const resultName = (namePerson === 'Артём') ? 'Директор' :
-  (namePerson === 'Артем') ? 'Директор' :
-  (namePerson === 'Максим') ? 'Преподаватель' : 'Студент';
-console.log(resultName);
+console.log(strChange(strInput));
