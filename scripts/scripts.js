4f516a92d579414f1b616f321d7bd58d7666c855
Rootdiv <rootdiv@mail.ru>
2021-02-03 14:20:04 +0300

    fix: Lesson04 Complicated task

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,20 @@
 //Усложнённое задание
 'use strict';
 const strInput = prompt('Введите произвольный текст');
+const notStr = 'Это не строка';
+const strReg = /\D/;
 
 function strChange(str) {
   if (typeof str === 'string' && str !== '') {
-    if (str.length > 30) {
-      return str.substring(0, 30).trim().concat('...');
+    if (!strReg.test(str)) {
+      return notStr;
+    } else if (str.length > 30) {
+      return str.substring(0, 30).concat('...');
     } else {
       console.log('"' + str + '"');
       return '"' + str.trim() + '"';
     }
   }
-  return 'Это не строка';
+  return notStr;
 }
 console.log(strChange(strInput));
