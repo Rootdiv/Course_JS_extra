912408c9efda78450aa138fe12f0d9643ada9a3d
Rootdiv <rootdiv@mail.ru>
2021-02-03 19:07:48 +0300

    feat: Lesson05 Complicated task

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,20 +1,20 @@
 //Усложнённое задание
 'use strict';
-const strInput = prompt('Введите произвольный текст');
-const notStr = 'Это не строка';
-const strReg = /\D/;
+const arr = ['1565452', '246545', '5115135', '4553135', '254535', '7115532', '463536', ];
+console.log('Выводим начинающееся с цифры 2 или 4');
+arr.forEach((item) => {
+  if (item[0] === '2' || item[0] === '4') {
+    console.log(item);
+  }
+});
 
-function strChange(str) {
-  if (typeof str === 'string' && str !== '') {
-    if (!strReg.test(str)) {
-      return notStr;
-    } else if (str.length > 30) {
-      return str.substring(0, 30).concat('...');
-    } else {
-      console.log('"' + str + '"');
-      return '"' + str.trim() + '"';
+console.log('Выводим простые числа от 1 до 100');
+const count = 100;
+nextPrime: for (let i = 2; i <= count; i++) {
+  for (let j = 2; j < i; j++) {
+    if (i % j === 0) {
+      continue nextPrime;
     }
   }
-  return notStr;
+  console.log('Делители числа ' + i + ': 1 и ' + i);
 }
-console.log(strChange(strInput));
