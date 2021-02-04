9a3a9cee612f3c5895604d34f9b4d54a96eac5e0
Rootdiv <rootdiv@mail.ru>
2021-02-04 16:16:34 +0300

    fix: Lesson04 You cannot enter only spaces Complicated task

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,10 +8,10 @@ function strChange(str) {
   if (typeof str === 'string' && str !== '') {
     if (!strReg.test(str)) {
       return notStr;
-    } else if (str.length <= 30) {
+    } else if (str.trim() !== '' && str.length <= 30) {
       console.log('"' + str + '"');
       return '"' + str.trim() + '"';
-    } else {
+    } else if (str.trim() !== '') {
       return str.substring(0, 30).concat('...');
     }
   }
