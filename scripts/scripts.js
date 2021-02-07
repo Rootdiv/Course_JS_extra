f04d02916b05cb45dde9250750468d035ba0ae84
Rootdiv <rootdiv@mail.ru>
2021-02-07 21:32:17 +0300

    style: Lesson07 Complicated task

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,6 +1,6 @@
 //Усложнённое задание
 'use strict';
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
+const week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
 const currentWeek = new Date().toLocaleString('ru', {
   weekday: 'long'
 });
@@ -8,10 +8,10 @@ const elem = document.querySelector('#lesson7');
 week.forEach((item, i) => {
   const day = document.createElement('div');
   day.textContent = week[i];
-  if (item.toLowerCase() === currentWeek) {
+  if (item === currentWeek) {
     day.classList.add('bold');
   }
-  if (item === 'Суббота' || item === 'Воскресенье') {
+  if (item === 'суббота' || item === 'воскресенье') {
     day.classList.add('italic');
   }
   elem.appendChild(day);
