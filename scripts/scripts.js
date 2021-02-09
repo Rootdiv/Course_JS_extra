9023233809335d393cc2fa80857b665c741c7577
Rootdiv <rootdiv@mail.ru>
2021-02-09 18:21:17 +0300

    feat: Lesson09 Complicated task

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,18 +1,102 @@
 //Усложнённое задание
 'use strict';
-const week = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
-const currentWeek = new Date().toLocaleString('ru', {
+
+function weekString(week) {
+  switch (week) {
+    case 0:
+      return 'Понедельник';
+    case 1:
+      return 'Вторник';
+    case 2:
+      return 'Среда';
+    case 3:
+      return 'Четверг';
+    case 4:
+      return 'Пятница';
+    case 5:
+      return 'Суббота';
+    case 6:
+      return 'Воскресенье';
+  }
+}
+
+function monthString(month) {
+  switch (month) {
+    case 0:
+      return 'января';
+    case 1:
+      return 'февраля';
+    case 2:
+      return 'марта';
+    case 3:
+      return 'апреля';
+    case 4:
+      return 'мая';
+    case 5:
+      return 'июня';
+    case 6:
+      return 'июля';
+    case 7:
+      return 'августа';
+    case 8:
+      return 'сентября';
+    case 9:
+      return 'октября';
+    case 10:
+      return 'ноября';
+    case 11:
+      return 'декабря';
+  }
+}
+
+function twoDigits(num) {
+  return ('0' + num).slice(-2);
+}
+
+function declOfNum(number, titles) {
+  const cases = [2, 0, 1, 1, 1, 2];
+  return titles[(number % 100 > 4 && number % 100 < 20) ? 2 : cases[(number % 10 < 5) ? number % 10 : 5]];
+}
+
+function hours(num) {
+  return declOfNum(num, ['час', 'часа', 'часов']);
+}
+
+function minutes(num) {
+  return declOfNum(num, ['минута', 'минуты', 'минут']);
+}
+
+function seconds(num) {
+  return declOfNum(num, ['секунда', 'секунды', 'секунд']);
+}
+
+const date = new Date();
+const shortDate = document.getElementById('short-date');
+const fullDate = document.getElementById('full-date');
+const week = new Date().toLocaleString('ru', {
   weekday: 'long'
 });
-const elem = document.querySelector('#lesson7');
-week.forEach((item, i) => {
-  const day = document.createElement('div');
-  day.textContent = week[i];
-  if (item === currentWeek) {
-    day.classList.add('bold');
-  }
-  if (item === 'суббота' || item === 'воскресенье') {
-    day.classList.add('italic');
-  }
-  elem.appendChild(day);
-});
+const curWeek = week.charAt(0).toUpperCase() + week.slice(1);
+const month = monthString(date.getMonth());
+const hour = hours(date.getHours());
+const minute = minutes(date.getHours());
+const second = seconds(date.getHours());
+
+function newDate() {
+  const time = new Date();
+  fullDate.textContent = `Сегодня ${curWeek}, ${twoDigits(time.getDate())} ${month} ${time.getFullYear()} года 
+  ${time.getHours()} ${hour} ${time.getMinutes()} ${minute} ${time.getSeconds()} ${second}`;
+}
+newDate();
+setInterval(function() {
+  return newDate();
+}, 1000);
+
+function currentDate() {
+  const time = new Date();
+  shortDate.textContent = time.toLocaleDateString() + ' - ' + time.toLocaleTimeString();
+}
+currentDate();
+setInterval(function() {
+  return currentDate();
+}, 1000);
