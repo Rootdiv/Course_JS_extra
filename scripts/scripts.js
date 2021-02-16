9f29107fe117aef797ae71a62f6f4935ed5dc2dc
Rootdiv <rootdiv@mail.ru>
2021-02-16 23:10:46 +0300

    feat: Lesson14 Complicated task

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,27 +1,58 @@
 //Усложнённое задание
 'use strict';
 
-const textColor = document.getElementById('text-color');
-const button = document.querySelector('button');
-
-function randColor() {
-  const red = Math.floor(Math.random() * 256);
-  const green = Math.floor(Math.random() * 256);
-  const blue = Math.floor(Math.random() * 256);
-  const color = '#' + red.toString(16) + green.toString(16) + blue.toString(16);
-  document.body.style.backgroundColor = color;
-  textColor.textContent = color;
-  button.style.color = color;
-  if (color === '#000000') {
-    button.backgroundColor = 'white';
-    textColor.style.color = 'white';
-  } else if (color === '#ffffff') {
-    button.style.backgroundColor = 'black';
-    textColor.style.color = 'black';
-  }
+function DomElement(selector, height, width, bg, fontSize, text) {
+  this.selector = selector;
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.fontSize = fontSize;
+  this.text = text;
+  this.position = 'absolute';
 }
 
-randColor();
-button.addEventListener('click', function() {
-  randColor();
-});
+const elem = document.createElement('div');
+DomElement.prototype.createElem = function() {
+  const one = this.selector[0];
+  if (one === '.') {
+    elem.className = this.selector.substring(1);
+  } else if (one === '#') {
+    elem.setAttribute('id', this.selector.substring(1));
+  }
+  elem.style.cssText = 'height: ' + this.height + '; width: ' + this.width + ';' +
+    ' background: ' + this.bg + '; font-size: ' + this.fontSize + '; position: ' + this.position + ';' +
+    ' left: ' + Math.round(window.innerWidth / 2 - parseInt(this.width) / 2) + 'px;' +
+    ' top: ' + Math.round(window.innerHeight / 2 - parseInt(this.height) / 2) + 'px;';
+  elem.textContent = this.text;
+  document.body.prepend(elem);
+};
+DomElement.prototype.moved = function(key) {
+  const top = parseInt(elem.style.top);
+  const left = parseInt(elem.style.left);
+  if (key === 'ArrowUp') {
+    elem.style.top = top - 10 + 'px';
+  } else if (key === 'ArrowRight') {
+    elem.style.left = left + 10 + 'px';
+  } else if (key === 'ArrowLeft') {
+    elem.style.left = left - 10 + 'px';
+  } else if (key === 'ArrowDown') {
+    elem.style.top = top + 10 + 'px';
+  }
+  if (top < 0) {
+    elem.style.top = 0;
+  } else if (left < 0) {
+    elem.style.left = 0;
+  }
+};
+DomElement.prototype.eventsListeners = function() {
+  const _this = this;
+  document.addEventListener('keydown', function(event) {
+    const key = event.code;
+    _this.moved(key);
+  });
+};
+
+const domElement = new DomElement('#elem', '100px', '100px', 'green', '24px', 'Элемент класса');
+domElement.eventsListeners();
+
+document.addEventListener('DOMContentLoaded', domElement.createElem());
