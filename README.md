9c7837f019db3033ba2545401a3837c6419d85a0
Rootdiv <rootdiv@mail.ru>
2021-03-09 21:22:54 +0300

    feat: Lesson29 Complicated task

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Course_JS
 Курс JavaScript от Glo Academy
 <div>
-  Урок &#8470;Урок 28<br>
-  Асинхронный JavaScript, Promise
+  Урок &#8470;Урок 29<br>
+  Fetch
 </div>
--- script.js
+++ script.js
@@ -1,42 +1,83 @@
 //Усложнённое задание
 'use strict';
 
-const select = document.getElementById('cars');
-const output = document.getElementById('output');
+const select = document.querySelector('.select');
+const usd = document.getElementById('usd');
+const eur = document.getElementById('eur');
+const dollar = document.querySelector('.dollar');
+const euro = document.querySelector('.euro');
+const direction = document.getElementById('direction');
+const btn = document.querySelector('button');
+const rubInput = document.querySelector('.rub-input');
+const usdInput = document.querySelector('.usd-input');
+const eurInput = document.querySelector('.eur-input');
 
 const getData = (url) => {
-  return new Promise((resolve, reject) => {
-    const request = new XMLHttpRequest();
-    request.open('GET', url);
-    request.setRequestHeader('Content-type', 'application/json');
-    request.addEventListener('readystatechange', () => {
-      if (request.readyState !== 4) {
-        return;
-      }
-      if (request.status === 200) {
-        const response = JSON.parse(request.responseText);
-        resolve(response);
-      } else {
-        reject(request.statusText);
-      }
-    });
-    request.send();
+  return fetch(url, {
+    mode: 'cors'
   });
 };
-const cars = (data) => {
-  data.cars.forEach(item => {
-    if (item.brand === select.value) {
-      const {
-        brand,
-        model,
-        price
-      } = item;
-      output.innerHTML = `Тачка ${brand} ${model} <br> Цена: ${price}$`;
-    } else if (select.value === 'no') {
-      output.textContent = 'Тачка не выбрана';
+const convert = (data) => {
+  const eurValue = data.rates.EUR;
+  const usdValue = data.rates.USD;
+  if (direction.checked) { //Конвертируем валюту в рубли
+    if (usdInput.value.trim() !== '' && usd.checked) {
+      //Конвертируем доллары в рубли
+      const usdToRub = +usdInput.value / usdValue;
+      rubInput.value = usdToRub.toFixed(2);
+    } else if (eurInput.value.trim() !== '' && eur.checked) {
+      //Конвертируем евро в рубли
+      const eurToRub = +eurInput.value / eurValue;
+      rubInput.value = eurToRub.toFixed(2);
     }
+  } else { //Конвертируем рубли в валюту
+    if (rubInput.value.trim() !== '' && usd.checked) {
+      //Конвертируем рубли в доллары
+      const usdToRub = +rubInput.value * usdValue;
+      usdInput.value = usdToRub.toFixed(2);
+    } else if (rubInput.value.trim() !== '' && eur.checked) {
+      //Конвертируем рубли в евро
+      const eurToRub = +rubInput.value * eurValue;
+      eurInput.value = eurToRub.toFixed(2);
+    }
+  }
+};
+const result = () => {
+  getData('https://api.exchangeratesapi.io/latest?base=RUB&symbols=USD,EUR').then((response) => {
+    if (response.status !== 200) {
+      throw new Error('Status network not 200');
+    }
+    return response.json();
+  }).then(data => convert(data)).catch(error => console.error(error));
+};
+const validInput = () => {
+  const inputText = document.querySelectorAll('input[type="text"]');
+  inputText.forEach(item => {
+    item.addEventListener('input', () => {
+      item.value = item.value.replace(/[\D]/g, '');
+    });
   });
 };
-select.addEventListener('change', () => {
-  getData('./cars.json').then(cars).catch(error => output.textContent = 'Произошла ошибка: ' + error);
+validInput();
+select.addEventListener('click', () => {
+  if (usd.checked) {
+    dollar.style.display = 'inline-block';
+    euro.style.display = 'none';
+    eurInput.value = '';
+  } else {
+    euro.style.display = 'inline-block';
+    dollar.style.display = 'none';
+    usdInput.value = '';
+  }
+  if (direction.checked) {
+    rubInput.value = '';
+    eurInput.value = '';
+    usdInput.value = '';
+  }
+});
+btn.addEventListener('click', result);
+document.addEventListener('keyup', (event) => {
+  if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+    result();
+  }
 });
