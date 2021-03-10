c6b072298f7b881d60f35f47a88b8fd01ab9e5ea
Rootdiv <rootdiv@mail.ru>
2021-03-10 18:32:36 +0300

    fix: Lesson26 Complicated task

--- script/script.js
+++ script/script.js
@@ -53,16 +53,17 @@ window.addEventListener('DOMContentLoaded', () => {
       menu.classList.toggle('active-menu');
     };
     document.body.addEventListener('click', (event) => {
-      let target = event.target;
+      const target = event.target;
       if (target.closest('.menu')) {
-        handlerMenu();
+        handlerMenu(); //Вызов меню по кнопке Меню
       } else if (target.classList.contains('close-btn')) {
-        handlerMenu();
+        handlerMenu(); //Закрытие меню по кнопке close
       } else {
-        target = target.matches('[href^="#"]');
-        if (target) {
+        //Блокируем вызов меню на стрелках слайдера и закрываем при клике на пунктах меню
+        if (target.matches('[href^="#"]') && !target.closest('.portfolio-content')) {
           handlerMenu();
         } else if (target && !target.closest('.menu')) {
+          //Закрываем меню при клике мимо или пункта меню
           menu.classList.remove('active-menu');
         }
       }
